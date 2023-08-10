b77291ebc82e45614225bff353b5096c9b51c29e
ASSIMv3 <hassanassim01@gmail.com>
2023-08-10 06:36:41 -0700

    adding amenity.py file

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines the Amenity class, which inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """A class representing an amenity in the AirBnB clone project"""
+    name = '
