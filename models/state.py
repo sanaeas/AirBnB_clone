e6b255044a188f03f01bc691fccd41a15f4ae511
ASSIMv3 <hassanassim01@gmail.com>
2023-08-10 06:34:54 -0700

    adding state.py file

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the State class, which inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """A class representing a state in the AirBnB clone project"""
+    name = ''
